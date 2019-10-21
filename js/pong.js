964da0b81dc89a8350b50b81a31309c8ff7c19fa
GuillaumeSavin <guisav1996@hotmail.fr>
2019-10-21 16:30:51 +0200

    Rectangle + balle

--- js/pong.js
+++ js/pong.js
@@ -7,4 +7,26 @@ function draw()
 
     ctx.fillStyle = "rgb(0,0,0)";
     ctx.fillRect(0,0, canvas.width, canvas.height);
+    context.beginPath();
+    context.fillStyle = 'white';
+    context.arc(game.ball.x, game.ball.y, game.ball.r, 0, Math.PI * 2, false);
+    context.fill();
 }
+
+document.addEventListener('DOMContentLoaded', function ()
+{
+    canvas = document.getElementById("canvas");
+
+
+    game=
+        {
+            ball:
+                {
+                    x: canvas.width / 2,
+                    y: canvas.height / 2,
+                    r: 5
+                }
+        }
+
+    draw();
+});
