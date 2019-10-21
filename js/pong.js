8166472c6b1d3bef4b0a4bdba4491db34214ae64
GuillaumeSavin <guisav1996@hotmail.fr>
2019-10-21 16:49:56 +0200

    Mouvement de la balle

--- js/pong.js
+++ js/pong.js
@@ -1,22 +1,30 @@
 var canvas;
+var game;
 
 function draw()
 {
     //var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
 
-    ctx.fillStyle = "rgb(0,0,0)";
+    ctx.fillStyle = "black";
     ctx.fillRect(0,0, canvas.width, canvas.height);
-    context.beginPath();
-    context.fillStyle = 'white';
-    context.arc(game.ball.x, game.ball.y, game.ball.r, 0, Math.PI * 2, false);
-    context.fill();
+    ctx.beginPath();
+    ctx.fillStyle = 'white';
+    ctx.arc(game.ball.x, game.ball.y, game.ball.r, 0, Math.PI * 2, false);
+    ctx.fill();
+}
+
+function play()
+{
+    game.ball.x += game.ball.speed.x;
+    game.ball.y += game.ball.speed.y;
+    draw();
+    requestAnimationFrame(play);
 }
 
 document.addEventListener('DOMContentLoaded', function ()
 {
     canvas = document.getElementById("canvas");
-
 
     game=
         {
@@ -24,9 +32,16 @@ document.addEventListener('DOMContentLoaded', function ()
                 {
                     x: canvas.width / 2,
                     y: canvas.height / 2,
-                    r: 5
+                    r: 5,
+                    speed:
+                        {
+                            x: 2,
+                            y: 2
+                        }
                 }
         }
-
+    game.ball.x += 2;
+    game.ball.y += 2;
     draw();
+    play();
 });
