44df4d25385e0585e398881ab876ddd38e240666
GuillaumeSavin <guisav1996@hotmail.fr>
2019-11-21 18:11:34 +0100

    2 balles + collisions balles

--- js/pong.js
+++ js/pong.js
@@ -25,6 +25,21 @@ function play()
 
 function move()
 {
+    /*
+    //TEST gestion collision, hitbox en carré
+    if (game.ball.x + game.ball.r <= game.ball2.x + game.ball2.r)
+    {
+        game.ball.speed.x = (game.ball.speed.x * -1);
+    }
+
+    if (game.ball.y + game.ball.r <= game.ball2.y + game.ball2.r)
+    {
+        game.ball.speed.y = (game.ball.speed.y * -1);
+    }
+
+     */
+
+    //Tester collisions mur ball made in Pantz
     if((game.ball.x < 0) && (game.ball.speed.x < 0))
     {
         game.ball.speed.x = (game.ball.speed.x * -1);
@@ -42,10 +57,12 @@ function move()
         game.ball.speed.y =(game.ball.speed.y * -1);
     }
 
+
+    //Tester bouger ball
     game.ball.x = (game.ball.x + game.ball.speed.x);
     game.ball.y = (game.ball.y + game.ball.speed.y);
 
-    //Test bouger ball2
+    //Tester collisions mur ball2 made in Pantz
     if((game.ball2.x < 0) && (game.ball2.speed.x < 0))
     {
         game.ball2.speed.x = (game.ball2.speed.x * -1);
@@ -63,8 +80,51 @@ function move()
         game.ball2.speed.y =(game.ball2.speed.y * -1);
     }
 
+    //Test bouger ball2
     game.ball2.x = (game.ball2.x + game.ball2.speed.x);
     game.ball2.y = (game.ball2.y + game.ball2.speed.y);
+
+    //Collisions deux cercles (exemple)
+    /*
+    bool Collision(Cercle C1,Cercle C2)
+    {
+        int d2 = (C1.x-C2.x)*(C1.x-C2.x) + (C1.y-C2.y)*(C1.y-C2.y);
+        if (d2 > (C1.rayon + C2.rayon)*(C1.rayon + C2.rayon))
+            return false;
+        else
+            return true;
+    }
+
+    //TEST COLLISION NOS BALLES
+    bool Collision()
+    {
+        d2 = (game.ball.x - game.ball2.x) * (game.ball.x - game.ball2.x) + (game.ball.y - game.ball2.y) * (game.ball.y - game.ball2.y);
+        if (d2 > (game.ball.r + game.ball2.r) * (game.ball.r + game.ball2.r))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    */
+
+
+    //TEST COLLISION NOS BALLES EN REEL
+    d2 = (game.ball.x - game.ball2.x) * (game.ball.x - game.ball2.x) + (game.ball.y - game.ball2.y) * (game.ball.y - game.ball2.y);
+    if (d2 > (game.ball.r + game.ball2.r) * (game.ball.r + game.ball2.r))
+    {
+
+    }
+    else
+    {
+        game.ball.speed.x = (game.ball.speed.x * -1);
+        game.ball.speed.y = (game.ball.speed.y * -1);
+        game.ball2.speed.x = (game.ball2.speed.x * -1);
+        game.ball2.speed.y = (game.ball2.speed.y * -1);
+    }
+
 
     /* code fonctionnel
     if (game.ball.x < canvas.width && game.ball.x > 0 && game.ball.y < canvas.height && game.ball.y > 0)
@@ -121,7 +181,7 @@ document.addEventListener('DOMContentLoaded', function ()
                 {
                     x: canvas.width / 2,
                     y: canvas.height / 2,
-                    r: 5,
+                    r: 10,
                     speed:
                         {
                             x: 10,
@@ -130,8 +190,8 @@ document.addEventListener('DOMContentLoaded', function ()
                 },
             ball2:
                 {
-                    x: 20,
-                    y: 20,
+                    x: 200,
+                    y: 200,
                     r: 10,
                     speed:
                         {
