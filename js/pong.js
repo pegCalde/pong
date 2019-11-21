8226f53c5b7693610fc415bce23ed4391abbf545
GuillaumeSavin <guisav1996@hotmail.fr>
2019-11-21 17:41:28 +0100

    2ieme balle

--- js/pong.js
+++ js/pong.js
@@ -11,6 +11,7 @@ function draw()
     ctx.beginPath();
     ctx.fillStyle = 'white';
     ctx.arc(game.ball.x, game.ball.y, game.ball.r, 0, Math.PI * 2, false);
+    ctx.arc(game.ball2.x, game.ball2.y, game.ball2.r, 0, Math.PI * 2, false);
     ctx.fill();
 }
 
@@ -43,6 +44,27 @@ function move()
 
     game.ball.x = (game.ball.x + game.ball.speed.x);
     game.ball.y = (game.ball.y + game.ball.speed.y);
+
+    //Test bouger ball2
+    if((game.ball2.x < 0) && (game.ball2.speed.x < 0))
+    {
+        game.ball2.speed.x = (game.ball2.speed.x * -1);
+    }
+    if((game.ball2.x > canvas.width) && (game.ball2.speed.x > 0))
+    {
+        game.ball2.speed.x = (game.ball2.speed.x * -1);
+    }
+    if((game.ball2.y < 0) && (game.ball2.speed.y < 0))
+    {
+        game.ball2.speed.y = (game.ball2.speed.y * -1);
+    }
+    if((game.ball2.y > canvas.height) && (game.ball2.speed.y > 0))
+    {
+        game.ball2.speed.y =(game.ball2.speed.y * -1);
+    }
+
+    game.ball2.x = (game.ball2.x + game.ball2.speed.x);
+    game.ball2.y = (game.ball2.y + game.ball2.speed.y);
 
     /* code fonctionnel
     if (game.ball.x < canvas.width && game.ball.x > 0 && game.ball.y < canvas.height && game.ball.y > 0)
@@ -104,6 +126,17 @@ document.addEventListener('DOMContentLoaded', function ()
                         {
                             x: 10,
                             y: 10
+                        }
+                },
+            ball2:
+                {
+                    x: 20,
+                    y: 20,
+                    r: 10,
+                    speed:
+                        {
+                            x: 5,
+                            y: 5
                         }
                 }
         }
